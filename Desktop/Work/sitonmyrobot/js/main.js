6135b3a7d19da9736a01568e91ef1815ffb4c739
KamilStaszewski <kamilstaszewski95@gmail.com>
2018-08-09 18:54:08 +0200

    beginning of sidenavbar on mobile

--- Desktop/Work/sitonmyrobot/js/main.js
+++ Desktop/Work/sitonmyrobot/js/main.js
@@ -2,19 +2,28 @@ var btn = document.querySelector('#menu-trigger');
 var menu = document.querySelector('.header__navigation');
 var mobile = window.matchMedia("screen and (min-width: 1024px)")
 
-btn.addEventListener('click', function(ev) {
+btn.addEventListener('click', function (ev) {
   ev.preventDefault();
   menu.classList.toggle('open');
 });
 
-mobile.addListener( function(mobile) {
+mobile.addListener(function (mobile) {
   if (mobile.matches) {
     menu.classList.remove('open');
   }
 });
 
-mobile.addListener( function(mobile) {
-    if (!mobile.matches) {
-      menu.classList.add('open');
-    }
-  });
+mobile.addListener(function (mobile) {
+  if (!mobile.matches) {
+    menu.classList.add('open');
+  }
+});
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
